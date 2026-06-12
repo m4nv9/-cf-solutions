86960136ed950afb42ddadec094a116e796166ef
Manvi Sharma <manvi7106@gmail.com>
2026-06-13 00:37:43 +0530

    executables

---
+++ week-5/d.cpp
@@ -0,0 +1,14 @@
+//search 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m; cin>>n>>m;
+
+    return 0;
+}
