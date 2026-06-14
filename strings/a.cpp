4fbcc6cdcab57e276c5806f2f3ceaeddd1dc0ae5
Manvi Sharma <manvi7106@gmail.com>
2026-06-14 12:48:57 +0530

    char/ascii

---
+++ strings/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char c; cin>>c;
+    cout<<(int)c;
+
+    return 0;
+}
---
+++ strings/strings.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // char x,y; cin>>x>>y;
+    // if(x==y) cout<<"YES";
+    // else cout<<"NO";
+  //ascii
+  //a-97
+  //A-65
+  //0-48 (char form 0)
+  char x = 'a';
+  cout<<(int)x;
+  int y = 98;
+  cout<<(char)y;  
+    return 0;
+}
