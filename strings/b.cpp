0fe7cbba5a7618b6281eb4f760a005f7765399ca
Manvi Sharma <manvi7106@gmail.com>
2026-06-14 12:50:18 +0530

    ascii/char

---
+++ strings/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x; cin>>x;
+    cout<<(char)x;
+    return 0;
+}
