6c78086bbcd1fac48a69ec3fc01970b38ea7dd64
Manvi Sharma <manvi7106@gmail.com>
2026-06-14 13:51:19 +0530

    brothers

---
+++ strings/e.cpp
@@ -0,0 +1,16 @@
+//brothers
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string f1,l1,f2,l2; cin>>f1>>l1>>f2>>l2;
+    if(l1==l2) cout<<"YES";
+    else cout<<"NO";
+
+    return 0;
+}
